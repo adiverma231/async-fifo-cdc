39df46cec7579a6281f5d531be78edb3a6bd6fb1
adiverma231 <verma231@purdue.edu>
2026-06-08 13:40:03 -0400

    Edit

--- src/rtl/fifo/fifo_mem.sv
+++ src/rtl/fifo/fifo_mem.sv
@@ -1,4 +1,15 @@
 module fifo_mem #(
     parameter DATA_WIDTH = 32,
     parameter ADDR_WIDTH = 4
-)
+)(
+    input  wire                  wr_clk,
+    input  wire                  wr_en,
+    input  wire [ADDR_WIDTH-1:0] wr_addr,
+    input  wire [DATA_WIDTH-1:0] wr_data,
+
+    input  wire                  rd_clk,
+    input  wire [ADDR_WIDTH-1:0] rd_addr,
+    output reg  [DATA_WIDTH-1:0] rd_data
+);
+
+    
