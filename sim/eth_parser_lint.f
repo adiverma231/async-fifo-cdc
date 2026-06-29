f79fc30e1f269541c6d0ccf8e79fe380b106549e
adiverma231 <verma231@purdue.edu>
2026-06-29 11:35:44 -0400

    Sims

--- sim/eth_parser_lint.f
+++ sim/eth_parser_lint.f
@@ -2,4 +2,5 @@
 --lint-only
 --top-module eth_frame_parser
 -Wno-fatal
+src/rtl/top/pkg_defines.sv
 src/rtl/parser/eth_frame_parser.sv
---
+++ sim/ipv4_parser_lint.f
@@ -0,0 +1,6 @@
+--sv
+--lint-only
+--top-module ipv4_parser
+-Wno-fatal
+src/rtl/top/pkg_defines.sv
+src/rtl/parser/ipv4_parser.sv
